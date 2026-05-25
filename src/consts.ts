7673d0fc165a985154f3f99da70c9eb0ef063eb1
Natalie <natalieavenefranz@gmail.com>
2026-05-25 17:01:37 -0400

    componentsAdded

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Dangerous Angels Creative';
+export const SITE_DESCRIPTION = 'It is a trip';
