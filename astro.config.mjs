dba2d507218168c7cbf45102514970b32678cb46
Natalie <natalieavenefranz@gmail.com>
2026-05-26 22:21:41 -0400

    cssAdded

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,13 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from "@tailwindcss/vite";
+
+
 // https://astro.build/config
 export default defineConfig({
+    vite: {
+    plugins: [tailwindcss()],
+  },
   integrations: [mdx(), sitemap()]
 });
